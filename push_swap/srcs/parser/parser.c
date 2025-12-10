3031cd6418b7a5104c1e9b6ecbeb3647b544326e
Leonard Decavele <ldecavel@z2r9p6.42lyon.fr>
2025-12-10 11:05:51 +0100

    [wip]

--- push_swap/srcs/parser/parser.c
+++ push_swap/srcs/parser/parser.c
@@ -10,20 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+#include "libft.h"
+
 typedef enum e_errcode
 {
-	ERR_USAGE,
+	ERR_AC,
 }	t_errcode;
 
-void	handle_error(int errcode)
+static void	handle_error(int errcode)
 {
 	exit(errcode);
 }
 
+static short	get_mode(char *s)
+{
+	if (!ft_strcmp(s, "--simple"))
+		return (SIMPLE);
+	else if (!ft_strcmp(s, "--medium"))
+		return (MEDIUM);
+	else if (!ft_strcmp(s, "--complex"))
+		return (COMPLEX);
+	else if (!ft_strcmp(s, "--adaptive"))
+		return (ADAPTIVE);
+	else if (!ft_strcmp(s, "--bench"))
+		return (BENCH);
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
+	int		i;
+	t_info	info;
+
+	info = {0};
 	if (ac < 2)
-		handle_error(ERR_USAGE);
-		//error
+		handle_error(ERR_AC);
+	i = 1;
+	while (i < ac && i < 3)
+		info.flags |= get_mode(av[i++])
 	
 }
