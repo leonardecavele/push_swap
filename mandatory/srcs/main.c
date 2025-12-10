4d1c1ef289e397a05cfe968dc6a74e771c30829b
parad0xe <parad0xe@protonmail.com>
2025-12-10 23:17:33 +0100

    fix computation of disorder, refactor main

--- mandatory/srcs/main.c
+++ mandatory/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 13:06:22 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/12/10 20:27:21 by nlallema         ###   ########lyon.fr   */
+/*   Updated: 2025/12/10 23:16:02 by nlallema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 
 static double	_compute_disorder(t_stack *stack)
 {
-	int		mistakes;
-	int		total_pairs;
+	double	mistakes;
+	double	total_pairs;
 	t_node	*current;
 	t_node	*sub_current;
 
@@ -31,7 +31,7 @@ static double	_compute_disorder(t_stack *stack)
 	while (current->next != stack->a)
 	{
 		sub_current = current->next;
-		while (sub_current->next != stack->a)
+		while (sub_current != stack->a)
 		{
 			total_pairs++;
 			if (current->value > sub_current->value)
@@ -51,29 +51,9 @@ int	main(int ac, char **av)
 	t_stack	stack;
 
 	parse(ac, av, &info, &stack);
-	print_stack(&stack); // to del
-	pa(&stack);
-	print_stack(&stack);
-	sa(&stack);
-	print_stack(&stack);
-	pa(&stack);
-	print_stack(&stack);
-	pa(&stack);
-	print_stack(&stack);
-	pa(&stack);
-	print_stack(&stack);
-	pb(&stack);
-	print_stack(&stack);
-	ra(&stack);
-	print_stack(&stack);
-	pb(&stack);
-	print_stack(&stack);
-	rb(&stack);
-	print_stack(&stack);
-	rr(&stack);
-	print_stack(&stack);
-	rrr(&stack);
-	print_stack(&stack);
+	if (info.flags & (BENCH | ADAPTIVE))
+		info.disorder = 100. - (_compute_disorder(&stack) * 100.);
+	// choose and run algorithm
 	list_clear(&stack.a);
 	list_clear(&stack.b);
 	return (0);
