76c85498a2122bb123ce0a6139d3a5f3668ab4e5
Leonard Decavele <ldecavel@z2r9p8.42lyon.fr>
2025-12-11 18:34:38 +0100

    edit: names and structure

--- mandatory/srcs/algorithms/simple.c
+++ mandatory/srcs/algorithms/simple.c
@@ -1,9 +1,12 @@
 /* ************************************************************************** */
-/*                                                                            */ /*                                                        :::      ::::::::   */ /*   simple.c                                           :+:      :+:    :+:   */ /*                                                    +:+ +:+         +:+     */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   simple.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/11 14:56:40 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/12/11 14:57:34 by ldecavel         ###   ########.fr       */
+/*   Created: 2025/12/11 18:32:55 by ldecavel          #+#    #+#             */
+/*   Updated: 2025/12/11 18:34:20 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -13,25 +16,25 @@ static void	target_min(t_stack *stack, t_node **target, bool *direction)
 {
 	t_node	*current;
 	long	min;
-	int		min_i;
-	int		i;
+	int		target_index;
+	int		index;
 
 	min = LONG_MAX;
-	min_i = 0;
-	i = 0;
+	target_index = 0;
+	index = 0;
 	current = stack->a;
-	while (current != stack->a || i == 0)
+	while (current != stack->a || index == 0)
 	{
 		if ((long)current->value < min)
 		{
-			min_i = i;
+			target_index = index;
 			min = current->value;
 			*target = current;
 		}
 		current = current->next;
-		i++;
+		index++;
 	}
-	if (stack->size_a - min_i > min_i)
+	if (stack->size_a - target_index > target_index)
 		*direction = RIGHT;
 	else
 		*direction = LEFT;
