059bee3f858ad1013845ad0853efe1b6c7ced06a
leonardecavele <leonard.decavele@gmail.com>
2025-12-15 12:41:04 +0100

    edit: brought the old 'simple' algorithm back

--- mandatory/srcs/algorithms/simple.c
+++ mandatory/srcs/algorithms/simple.c
@@ -6,14 +6,55 @@
 /*   By: ldecavel <ldecavel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 15:01:38 by ldecavel          #+#    #+#             */
-/*   Updated: 2025/12/15 12:35:37 by ldecavel         ###   ########.fr       */
+/*   Updated: 2025/12/15 12:40:31 by ldecavel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "algorithms.h"
 
+static void	target_min(t_stack *stack, t_node **target, bool *direction)
+{
+	t_node	*current;
+	long	min;
+	int		target_index;
+	int		index;
+
+	min = LONG_MAX;
+	target_index = 0;
+	index = 0;
+	current = stack->a;
+	while (current != stack->a || index == 0)
+	{
+		if ((long)current->value < min)
+		{
+			target_index = index;
+			min = current->value;
+			*target = current;
+		}
+		current = current->next;
+		index++;
+	}
+	if (stack->size_a - target_index > target_index)
+		*direction = RIGHT;
+	else
+		*direction = LEFT;
+}
+
 extern void	simple(t_stack *stack, t_info *info)
 {
-	(void)stack;
-	(void)info;
+	t_node	*target;
+	bool	direction;
+
+	while (stack->size_a)
+	{
+		target_min(stack, &target, &direction);
+		while (stack->a != target)
+			if (direction == RIGHT)
+				ra(stack, info);
+			else if (direction == LEFT)
+				rra(stack, info);
+		pb(stack, info);
+	}
+	while (stack->b)
+		pa(stack, info);
 }
